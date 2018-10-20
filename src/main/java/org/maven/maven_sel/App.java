6760d8e8194e99896a3c6813e822355433d6c3f2
Avinash <avinash.sharma@aricent.com>
2018-10-20 16:15:36 +0530

    second commit

--- src/main/java/org/maven/maven_sel/App.java
+++ src/main/java/org/maven/maven_sel/App.java
@@ -32,21 +32,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
 	@Test
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/home/edureka/chromedriver");
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
-		driver.get("http://localhost:8090/addressbook");
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.findElement(By.className("v-button")).click();
-		driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
-		driver.findElement(By.id("gwt-uid-7")).sendKeys("DevOps");
-		driver.findElement(By.id("gwt-uid-9")).sendKeys("9834562709");
-		driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edureka.co");
-		driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2015");
-		driver.findElement(By.className("v-button-primary")).click();
-		// Thread.sleep(5000);
+		driver.get("http://localhost:3001/");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
+		driver.findElement(By.name("login")).sendKeys("devops");
+		driver.findElement(By.name("password")).sendKeys("test");
+		driver.findElement(By.name("click")).click();		
+		Thread.sleep(5000);
 		driver.quit();
 	}
 }
